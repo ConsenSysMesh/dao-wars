6a6577fb008fe944d651576a9e4f7c93799813f1
Peter Borah <peterborah@gmail.com>
2014-11-18 22:07:14 -0600

    Only allow admin to change things.

--- tests/square_test.py
+++ tests/square_test.py
@@ -5,6 +5,7 @@ from pyethereum import tester as t
 class TestSquare:
     def setup(self):
         self.contract = Contract("contracts/square.se")
+        self.contract.call(13, [])
 
     def test_down(self):
         self.contract.call(1, [t.a0])
@@ -12,4 +13,9 @@ class TestSquare:
 
     def test_ether(self):
         self.contract.call(9, [1000])
+        assert_equal(self.contract.call(8,[]), [1000])
+
+    def test_only_allows_admin_to_set(self):
+        self.contract.call(9, [1000])
+        self.contract.call(9, [1001], sender=t.k1)
         assert_equal(self.contract.call(8,[]), [1000])
