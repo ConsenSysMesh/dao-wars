80e7e7b2edffabd990f73bc7d6cb72a36c34b890
Peter Borah <peterborah@gmail.com>
2014-11-19 12:56:33 -0600

    Clean up state setting

--- tests/body_test.py
+++ tests/body_test.py
@@ -2,22 +2,26 @@ from eth_tools import Contract, address
 from nose.tools import assert_equal
 from pyethereum import tester as t
 
+REWRITE_STATE = 0
+MOVE_LEFT = 1
+GET_LOCATION = 5
+
+SQUARE_REWRITE_STATE = 0
+
 class TestBody:
     def setup(self):
         self.state = t.state()
         self.contract = Contract("contracts/body.se", self.state)
-        self.contract.call(0, [])
 
     def test_move_left(self):
         location = Contract("contracts/square.se", self.state)
         neighbor = Contract("contracts/square.se", self.state)
 
-        location.call(13, [])
-        location.call(1, [neighbor.contract])
-        self.contract.call(6, [location.contract])
+        location.call(SQUARE_REWRITE_STATE, [neighbor.contract, 0, 0, 0, 0, 0, t.a0])
+        self.contract.call(REWRITE_STATE, [location.contract, 0, 0, 0, 0, t.a0])
 
-        assert_equal(address(self.contract.call(5, [])[0]), location.contract)
+        assert_equal(address(self.contract.call(GET_LOCATION, [])[0]), location.contract)
 
-        self.contract.call(1, [])
+        self.contract.call(MOVE_LEFT, [])
 
-        assert_equal(address(self.contract.call(5, [])[0]), neighbor.contract)
+        assert_equal(address(self.contract.call(GET_LOCATION, [])[0]), neighbor.contract)
--- tests/square_test.py
+++ tests/square_test.py
@@ -2,21 +2,28 @@ from eth_tools import Contract, address
 from nose.tools import assert_equal
 from pyethereum import tester as t
 
+REWRITE_STATE = 0
+GET_LEFT = 1
+GET_ETHER = 5
+GET_ADMIN = 7
+
 class TestSquare:
     def setup(self):
         self.state = t.state()
         self.contract = Contract("contracts/square.se", self.state)
-        self.contract.call(13, [])
 
     def test_left(self):
-        self.contract.call(1, [t.a0])
-        assert_equal(address(self.contract.call(0, [])[0]), t.a0)
+        self.contract.call(REWRITE_STATE, [t.a1, 0, 0, 0, 0, 0, 0])
+        assert_equal(address(self.contract.call(GET_LEFT, [])[0]), t.a1)
 
     def test_ether(self):
-        self.contract.call(9, [1000])
-        assert_equal(self.contract.call(8,[]), [1000])
+        self.contract.call(REWRITE_STATE, [0, 0, 0, 0, 1000, 0, 0])
+        assert_equal(self.contract.call(GET_ETHER,[]), [1000])
 
-    def test_only_allows_admin_to_set(self):
-        self.contract.call(9, [1000])
-        self.contract.call(9, [1001], sender=t.k1)
-        assert_equal(self.contract.call(8,[]), [1000])
+    def test_locks_changes_to_non_admins(self):
+        self.contract.call(REWRITE_STATE, [0, 0, 0, 0, 1000, 0, t.a0])
+        assert_equal(address(self.contract.call(GET_ADMIN, [])[0]), t.a0)
+
+        self.contract.call(REWRITE_STATE, [0, 0, 0, 0, 1001, 0, t.a0])
+        self.contract.call(REWRITE_STATE, [0, 0, 0, 0, 1002, 0, 0], sender=t.k1)
+        assert_equal(self.contract.call(GET_ETHER,[]), [1001])
