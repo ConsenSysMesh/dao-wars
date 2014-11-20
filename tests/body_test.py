f6b6b068bb3c386a9ca86424fca1e10ed9abc24e
Peter Borah <peterborah@gmail.com>
2014-11-19 20:59:10 -0600

    Allow body to harvest ether.

--- tests/body_test.py
+++ tests/body_test.py
@@ -5,8 +5,11 @@ from pyethereum import tester as t
 REWRITE_STATE = 0
 MOVE_LEFT = 1
 GET_LOCATION = 5
+GET_ETHER = 6
+HARVEST = 11
 
 SQUARE_REWRITE_STATE = 0
+SQUARE_GET_ETHER = 5
 SQUARE_GET_CREATURE = 6
 
 class TestBody:
@@ -29,3 +32,19 @@ class TestBody:
         assert_equal(address(self.contract.call(GET_LOCATION)[0]), neighbor.contract)
         assert_equal(location.call(SQUARE_GET_CREATURE), [0])
         assert_equal(address(neighbor.call(SQUARE_GET_CREATURE)[0]), self.contract.contract)
+
+    def test_harvest(self):
+        location = Contract("contracts/square.se", self.state)
+        location.call(SQUARE_REWRITE_STATE, [0, 0, 0, 0, 150, self.contract.contract, t.a0])
+        self.contract.call(REWRITE_STATE, [location.contract, 0, 0, 0, 0, t.a0])
+
+        assert_equal(location.call(SQUARE_GET_ETHER), [150])
+        assert_equal(self.contract.call(GET_ETHER), [0])
+
+        assert_equal(self.contract.call(HARVEST), [100])
+        assert_equal(location.call(SQUARE_GET_ETHER), [50])
+        assert_equal(self.contract.call(GET_ETHER), [100])
+
+        self.contract.call(HARVEST)
+        assert_equal(location.call(SQUARE_GET_ETHER), [0])
+        assert_equal(self.contract.call(GET_ETHER), [150])
