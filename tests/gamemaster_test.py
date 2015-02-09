7868c4b9d1088261e2e72c84c0f2ff1c5d19f3ee
Peter Borah <peterborah@gmail.com>
2015-02-09 13:08:05 -0500

    Create basic gamemaster contract.

---
+++ tests/gamemaster_test.py
@@ -0,0 +1,17 @@
+from eth_tools import address
+from nose.tools import assert_equal
+from pyethereum import tester as t
+
+class TestGamemaster:
+    def setup(self):
+        self.state = t.state()
+        self.contract = self.state.abi_contract("contracts/gamemaster.se")
+
+    def test_can_create_game(self):
+        player_list = [3, 4]
+        self.contract.rewrite_state(player_list, 100)
+
+        assert_equal(self.contract.get_player_list(outsz=2), player_list)
+        assert_equal(self.contract.get_num_players(), [2])
+        assert_equal(self.contract.get_move_limit(), [100])
+
