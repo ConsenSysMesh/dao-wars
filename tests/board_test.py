e347f5dad5ce3a600ef30c470a1e4c04838e91e6
Peter Borah <peterborah@gmail.com>
2015-02-25 18:23:39 -0500

    Allow gameboard to be initialized one step at a time.

--- tests/board_test.py
+++ tests/board_test.py
@@ -7,5 +7,12 @@ class TestBoard:
         self.contract = self.state.abi_contract("contracts/board.se")
 
     def test_it_does_not_blow_up(self):
-        self.contract.create_board(10,10)
+        self.contract.set_dimensions(10,10)
+        self.contract.create_board()
+        assert_not_equal(self.contract.get_square(0,0), 0)
+        assert_not_equal(self.contract.get_square(0,1), 0)
+        assert_not_equal(self.contract.get_square(1,0), 0)
         assert_not_equal(self.contract.get_square(9,9), 0)
+
+        assert_equal(self.contract.get_square(10,9), 0)
+        assert_equal(self.contract.get_square(9,10), 0)
