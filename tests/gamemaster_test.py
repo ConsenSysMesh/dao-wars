dbb4f625358d0ee82e0fcc0f4b0c0fb698c7b40d
Peter Borah <peterborah@gmail.com>
2015-02-26 12:04:01 -0500

    Small changes for testnet testing.

--- tests/gamemaster_test.py
+++ tests/gamemaster_test.py
@@ -8,21 +8,20 @@ class TestGamemaster:
         self.contract = self.state.abi_contract("contracts/gamemaster.se")
 
     def test_can_create_game(self):
-        creature_list = [3, 4]
-        self.contract.set_creatures(creature_list)
+        self.contract.set_creatures(3,4)
         self.contract.set_turn_limit(100)
 
-        assert_equal(self.contract.get_creatures(), creature_list)
+        assert_equal(self.contract.get_creatures(), [3,4])
         assert_equal(self.contract.get_num_creatures(), 2)
         assert_equal(self.contract.get_turn_limit(), 100)
 
     def test_only_creator_can_set_creatures_or_turn_limit(self):
         creature_list = [3, 4]
-        self.contract.set_creatures(creature_list)
+        self.contract.set_creatures(3,4)
         self.contract.set_turn_limit(100)
 
-        assert_equal(self.contract.set_creatures(creature_list, sender=t.k1), -1)
-        assert_equal(self.contract.get_creatures(), creature_list)
+        assert_equal(self.contract.set_creatures(4, 5, sender=t.k1), -1)
+        assert_equal(self.contract.get_creatures(), [3,4])
 
     def test_run_game_calls_down_to_brain_through_body(self):
         brain_1 = self.state.abi_contract("mocks/brain/counter.se")
@@ -38,7 +37,7 @@ class TestGamemaster:
         creature_2.set_brain(brain_2.address)
         creature_2.set_gamemaster(self.contract.address)
 
-        self.contract.set_creatures([creature_1.address, creature_2.address])
+        self.contract.set_creatures(creature_1.address, creature_2.address)
         self.contract.set_turn_limit(5)
         self.contract.run_game()
 
@@ -49,24 +48,24 @@ class TestGamemaster:
         child = self.state.abi_contract("mocks/body/counter.se")
         body = self.state.abi_contract("mocks/body/spawner.se")
         body.set_child_address(child.address)
-        self.contract.set_creatures([body.address])
+        self.contract.set_creatures(body.address, 0)
         self.contract.set_turn_limit(1)
 
         self.contract.run_game()
 
-        assert_equal(self.contract.get_num_creatures(), 2)
+        assert_equal(self.contract.get_num_creatures(), 3)
         assert_equal(child.get_num_turns(), 1)
 
     def test_only_acting_creature_can_spawn(self):
-        self.contract.set_creatures([t.a1])
+        self.contract.set_creatures(t.a1, 0)
         self.contract.set_turn_limit(5)
 
         assert_equal(self.contract.notify_of_spawn(t.a0), -1)
-        assert_equal(self.contract.get_num_creatures(), 1)
+        assert_equal(self.contract.get_num_creatures(), 2)
 
     def test_running_turn_charges_gas(self):
         body = self.state.abi_contract("mocks/body/cost_counter.se")
-        self.contract.set_creatures([body.address])
+        self.contract.set_creatures(body.address, 0)
         self.contract.set_turn_limit(1)
 
         self.contract.run_turn()
@@ -75,7 +74,7 @@ class TestGamemaster:
 
     def test_turn_only_takes_given_bodys_remaining_gas(self):
         body = self.state.abi_contract("mocks/body/bankrupt.se")
-        self.contract.set_creatures([body.address])
+        self.contract.set_creatures(body.address, 0)
         self.contract.set_turn_limit(3)
 
         assert_raises(Exception, self.contract.run_game)
