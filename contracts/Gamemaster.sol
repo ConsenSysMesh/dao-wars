9496963ffa81432d5a9085fb01d37cda9215da65
Peter Borah <peterborah@gmail.com>
2015-07-23 04:21:39 -0400

    Restrict gamemaster changes to creator.

--- contracts/Gamemaster.sol
+++ contracts/Gamemaster.sol
@@ -2,12 +2,23 @@ contract Gamemaster {
   address[] public creatures;
   address[] public squares;
   uint[2] public dimensions;
+  address public admin;
 
-  function set_creatures(address[] _creatures) {
+  modifier auth(address authorized_user) { if (msg.sender == authorized_user) _ }
+
+  function Gamemaster() {
+    admin = msg.sender;
+  }
+
+  function set_admin(address _admin) auth(admin) {
+    admin = _admin;
+  }
+
+  function set_creatures(address[] _creatures) auth(admin) {
     creatures = _creatures;
   }
 
-  function add_creature(address _new_creature) {
+  function add_creature(address _new_creature) auth(admin) {
     uint new_index = creatures.length;
     creatures.length++;
     creatures[new_index] = _new_creature;
@@ -17,12 +28,12 @@ contract Gamemaster {
     return creatures.length;
   }
 
-  function set_dimensions(uint x, uint y) {
+  function set_dimensions(uint x, uint y) auth(admin) {
     dimensions[0] = x;
     dimensions[1] = y;
   }
 
-  function set_squares(address[] _squares) {
+  function set_squares(address[] _squares) auth(admin) {
     squares = _squares;
   }
 }
