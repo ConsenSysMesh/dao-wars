eea792dea9230d975bebe12d3e5246952f5021dd
Peter Borah <peterborah@gmail.com>
2015-09-14 00:36:41 -0400

    Update launch script.

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -5,21 +5,14 @@
 // loadScript('initialize.js')
 
 loadScript('contracts.js')
-boilerplate = {from: "0xa1f4e5914033106a0f8bb4c2446c6cd050bec771", gas: 2000000}
+boilerplate = {from: eth.coinbase, gas: 2000000}
 
-board = eth.contract(contracts.Board.abi).at(contracts.Board.address)
-gm = eth.contract(contracts.Gamemaster.abi).at(contracts.Gamemaster.address)
-creature = eth.contract(contracts.Creature.abi).at(contracts.Creature.address)
-brain = eth.contract(contracts.BrainMock.abi).at(contracts.BrainMock.address)
+game = eth.contract(contracts.Game.abi).at(contracts.Game.address)
 
 miner.start()
-board.set_gamemaster(gm.address, boilerplate)
-gm.set_board(board.address, boilerplate)
-creature.set_board(board.address, boilerplate)
-creature.set_gamemaster(gm.address, boilerplate)
-creature.set_brain(brain.address, boilerplate)
-creature.set_gas(100000, boilerplate)
-board.set_dimensions(10, 10, boilerplate)
-board.spawn(20, creature.address, boilerplate)
-board.deposit_gas(30, 100000, boilerplate)
+game.initialize(10,10,10,1000000,boilerplate)
 admin.sleepBlocks(1)
+game.add_creature(contracts.Creature.address,boilerplate)
+admin.sleepBlocks(1)
+
+board = eth.contract(contracts.Board.abi).at(game.board())
